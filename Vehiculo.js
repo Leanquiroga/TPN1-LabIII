53e6ef3237074a969e34580d23e296d83420de7c
Leanquiroga <143456057+Leanquiroga@users.noreply.github.com>
2024-05-07 17:51:33 -0300

    Create Class Ve
    Clase Vehiculo, propiedades, set, get.

--- Vehiculo.js
+++ Vehiculo.js
@@ -1,7 +1,47 @@
 class Vehiculo{
+    #modelo;
+    #año;
+    #color;
+    #peso;
+    constructor(modelo, año, color, peso){
+        this.#modelo = modelo;
+        this.#año = año;
+        this.#color = color;
+        this.#peso = peso;
+    }
+
+    setModelo(modelo){
+        this.#modelo = modelo;
+    }
+    getModelo(){
+        return this.#modelo;
+    }
+
+    setAño(año){
+        this.#año = año;
+    }
+    getAño(){
+        return this.#año;
+    }
+
+    setColor(color){
+        this.#color = color;
+    }
+    getColor(){
+        return this.#color;
+    }
+
+    setPeso(peso){
+        this.#peso = peso;
+    }
+    getPeso(){
+        return this.#peso;
+    }
+
+
 
 }
 
 class Motocicleta extends Vehiculo{
-    
+
 }
